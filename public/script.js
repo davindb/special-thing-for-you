23a8157d9153e838e7045826976e5c9abe5c6dd4
davindb <davindb@Davins-MacBook-Pro.local>
2022-06-19 16:48:06 +0700

    init

---
+++ public/script.js
@@ -0,0 +1,37 @@
+function click_btn() {
+  //   $("#exampleModalCenter").hide();
+  //   $("#modal_2").modal("show");
+  jQuery.noConflict();
+  $("#modal_1").modal("hide");
+  $("#modal_2").modal("show");
+}
+
+function click_btn_3() {
+  //   $("#exampleModalCenter").hide();
+  //   $("#modal_2").modal("show");
+  jQuery.noConflict();
+  $("#modal_2").modal("hide");
+  $("#modal_3").modal("show");
+}
+
+function click_btn_final() {
+  //   $("#exampleModalCenter").hide();
+  //   $("#modal_2").modal("show");
+  jQuery.noConflict();
+  $("#modal_3").modal("hide");
+  $("#modal_4").modal("show");
+}
+
+function ditasari() {
+  jQuery.noConflict();
+  $("#modal_4").modal("hide");
+  setTimeout(function () {
+    document.location = "./index.html#sixthings";
+    $("html, body").animate(
+      {
+        scrollTop: $("#sixthings").offset().top,
+      },
+      2000
+    );
+  }, 500);
+}
